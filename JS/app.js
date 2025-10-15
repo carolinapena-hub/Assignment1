e15e0d40d09520b5145b92caeaf62dec3461b9c7
Digitalnomad <carolinapena@gmail.com>
2025-10-15 12:58:35 +0200

    Initial commit-added project structure
    Created the initial folder structure for the Toscana Reseguiden project.
    - Added index.html (HTML5 boilerplate)
    - Added css/style.css
    - Added js/app.js
    - Created images/ folder
    - Added README.md for documentation

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+// Enkel testfunktion
+console.log("Toscana Reseguiden laddad!");
+
+function showAlert() {
+    alert("Välkommen till Toscana!");
+}
+
+// Om du vill kan du koppla detta till en knapp i HTML
